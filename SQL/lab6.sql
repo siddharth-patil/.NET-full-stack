6de5efe0892903bbec4d10d7a84e24c6e067e88f
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-01-25 17:40:36 +0530

    lab questions

--- SQL/lab6.sql
+++ SQL/lab6.sql
@@ -24,3 +24,79 @@ from employees;
 select hire_date, to_char(hire_date,'day') as day
 from employees
 where last_name = 'Davies';
+------------------------------------------------------------------------------
+
+select employee_id ,job_id,  (case WHEN job_id = 'AD_PRESS' THEN 'A'
+                        WHEN job_id = 'ST_MAN' THEN 'A'
+                        WHEN job_id = 'IT_PROG' THEN 'A'
+                        WHEN job_id = 'SA_REP' THEN 'A'
+                        WHEN job_id = 'ST_CLERK' THEN 'A'
+                        ELSE '0'
+                        end) as grade
+FROM EMPLOYEES;    
+--------------------------------------------------------------------------------
+select employee_id ,job_id,  (case job_id WHEN 'AD_PRESS' THEN 'A'
+                        WHEN 'ST_MAN' THEN 'A'
+                        WHEN 'IT_PROG' THEN 'A'
+                        WHEN 'SA_REP' THEN 'A'
+                        WHEN 'ST_CLERK' THEN 'A'
+                        ELSE '0'
+                        end) as grade
+FROM EMPLOYEES; 
+-------------------------------------------------------------------------------
+select employee_id ,job_id,  DECODE(job_id , 'AD_PRESS' , 'A'
+                        , 'ST_MAN' , 'A'
+                        , 'IT_PROG' , 'A'
+                        , 'SA_REP' , 'A'
+                        , 'ST_CLERK' , 'A'
+                        , '0') as grade
+FROM EMPLOYEES;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
