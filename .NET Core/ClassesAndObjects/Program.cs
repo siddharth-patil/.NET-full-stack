3c7d25b717baff1e71d644247aea42cc31b45b5e
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-03-15 12:24:33 +0530

    practice

---
+++ .NET Core/ClassesAndObjects/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassesAndObjects;
+
+Console.WriteLine("Hello, World!");
+
+Student student1 = new Student();
+
+student1.GetInfo(123,"sid",98);
+student1.displayInfo();
---
+++ .NET Core/ClassesAndObjects/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    internal class Student
+    {
+        int rollNo;
+        string name;
+        double percentage;
+
+        public void GetInfo(int RollNo, string Name, double Percentage)
+        {
+            rollNo = RollNo;
+            name = Name;
+            percentage = Percentage;
+        }
+
+        public void displayInfo()
+        {
+            Console.WriteLine($"Student data:\n Roll Number:{rollNo}\n Name:{name}\n Percentage:{percentage}");
+        }
+    }
+}
