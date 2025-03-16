c52056295b07aac8386c717aadb939cf85d8d563
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-03-16 21:42:38 +0530

    amazon project main/hero section

---
+++ RWD/Amazon/script.js
@@ -0,0 +1,34 @@
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prevBtn = document.querySelector('.control_prev');
+const nextBtn = document.querySelector('.control_next');
+
+let n=0;
+
+function changeSlide(){
+    for(let i=0; i<imgs.length; i++){
+        imgs[i].style.display = 'none';
+    }
+    imgs[n].style.display = 'block';
+}
+
+changeSlide();
+
+prevBtn.addEventListener('click', (event)=>{
+    if(n>0){
+        n--;
+    }
+    else{
+        n = imgs.length - 1;
+    }
+    changeSlide();
+})
+
+nextBtn.addEventListener('click', (event)=>{
+    if(n < (imgs.length - 1)){
+        n++;
+    }
+    else{
+        n = 0;
+    }
+    changeSlide();
+})
