86e2576b820f3a1a948a3be851d306316264ec7f
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-07-04 22:04:52 +0530

    Angular files

---
+++ Angular/my-app/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'my-app';
+  title1 = 'Angular first app';
+  name = '.Net fullstack';
+}
