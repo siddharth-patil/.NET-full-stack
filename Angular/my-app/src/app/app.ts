a3fe696e7df3e806004faf3cc80baadce3e9cd0e
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-07-09 21:58:12 +0530

    angular interpolation and component generation

--- Angular/my-app/src/app/app.ts
+++ Angular/my-app/src/app/app.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from "./login/login";
+import { Signup } from "./signup/signup";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login, Signup],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'my-app';
-  title1 = 'Angular first app';
-  name = '.Net fullstack';
+  // protected title = 'my-app';
+  // title1 = 'Angular first app';
+  // name = '.Net fullstack';
+  // x=12;
+  // y=23;
+  // text1= "brother";
+  // text2= "bhava";
+  // p=true;
+  // q=true;
+ count:number = 0;
+  handleCounter(val:string){
+    if(val === 'minus'){
+      if(this.count>0){
+        this.count = this.count-1
+      }
+    }else if(val === 'plus'){
+        this.count = this.count+1;
+      }else if(val===''){
+        this.count=0;
+      }
+  }
+
 }
---
+++ Angular/my-app/src/app/signup/signup.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'signup',
+  imports: [],
+  templateUrl: './signup.html',
+  styleUrl: './signup.css'
+})
+export class Signup {
+
+}
