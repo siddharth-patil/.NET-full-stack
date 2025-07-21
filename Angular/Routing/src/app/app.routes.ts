fd4cc71f6a9b9a6d37f34edd4f92aa788494771c
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-07-21 22:50:03 +0530

    routing practice

--- Angular/Routing/src/app/app.routes.ts
+++ Angular/Routing/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Login } from './login/login';
+import { Contact } from './contact/contact';
+import { Home } from './home/home';
+import { PageNotFound } from './page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'about',component:About},
+    {path:'login',component:Login},
+    {path:'contact',component:Contact},
+    {path:'',component:Home},
+    {path:'**',component:PageNotFound},
+];
