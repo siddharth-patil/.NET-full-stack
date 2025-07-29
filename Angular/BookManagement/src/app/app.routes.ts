3fba01d49e676cb5a99b69d8256e722f560334c4
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-07-29 19:47:15 +0530

    book management app

--- Angular/BookManagement/src/app/app.routes.ts
+++ Angular/BookManagement/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AddBook } from './add-book/add-book';
 import { ShowBooks } from './show-books/show-books';
 
 export const routes: Routes = [
+    {path:'',component:Home},
     {path:'home',component:Home},
     {path:'add-book',component:AddBook},
     {path:'show-books',component:ShowBooks}
