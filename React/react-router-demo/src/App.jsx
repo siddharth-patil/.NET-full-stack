ce79af6bc39d99229e8eeba1aab551c008a6ed8f
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-09-05 23:59:09 +0530

    react-routing

--- React/react-router-demo/src/App.jsx
+++ React/react-router-demo/src/App.jsx
@@ -4,25 +4,22 @@ import Login from "./Login";
 import Home from "./Home";
 import Navbar from "./Navbar";
 
-
 function App() {
-
   return (
     <>
       {/* <Link to={"/"}>Home</Link>
       <Link to={"/about"}>About</Link>
       <Link to={"/login"}>Login</Link> */}
 
-        <Navbar></Navbar>
+      <Navbar></Navbar>
 
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/login" element={<Login/>}/>
-    </Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
-      
-  )
+  );
 }
 
-export default App
+export default App;
