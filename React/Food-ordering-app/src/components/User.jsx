eb7d22c4450f2f013a93586bce73c72f0f536b38
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-09-08 23:11:37 +0530

    fetch git hub api for about page

--- React/Food-ordering-app/src/components/User.jsx
+++ React/Food-ordering-app/src/components/User.jsx
@@ -1,41 +1,54 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 
-class User extends React.Component{
+class User extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
+    this.state = {
+      // count : 0,
+      userInfo: { name: "sid", location: "pun", bio: "bio", company: "tata", avatar_url:'#' },
+    };
+  }
 
-        this.state = {
-            count : 0,
-        }
-    }
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/siddharth-patil");
+    const json = await data.json();
+    console.log(json);
 
-    componentDidMount(){
-        
-    }
+    this.setState({
+        userInfo:json
+    });
+  }
 
+  render() {
+    // const { name, location, contact } = this.props;
+    // const { count } = this.state;
 
-    render(){
-        const {name, location, contact} = this.props;
-        const {count} = this.state;
-        return(
-            <div className="user-card">
-                <h1>Count : {count}</h1>
-                <button onClick={()=>{
-                    this.setState({
-                        count : this.state.count + 1,
-                    })
-                    // this.state.count = this.state.count + 1;
-                    console.log(this.state.count);
-                    
-                }}>Increase Count</button>
-                <h2>Name : {name}</h2>
-                <h2>Location : {location}</h2>
-                <h2>Contact : {contact}</h2>
-            </div>
-        )
-    }
+    const {name, location, company, bio, avatar_url} = this.state.userInfo;
 
+    return (
+      <div className="user-card">
+        {/* <h1>Count : {count}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+            // this.state.count = this.state.count + 1;
+            console.log(this.state.count);
+          }}
+        >
+          Increase Count
+        </button> */}
+        <img src={avatar_url} alt="" />
+        <h2>Name : {name}</h2>
+        <h2>Location : {location}</h2>
+        <h2>Contact : siddharth-patil @{company}</h2>
+        <h2>Bio : {bio}</h2>
+      </div>
+    );
+  }
 }
 
 export default User;
