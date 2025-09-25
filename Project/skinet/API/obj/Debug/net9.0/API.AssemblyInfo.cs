1051a1811d6fc1170db8fe384af912c711372eba
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-09-25 22:27:57 +0530

    skinet - added product.cs

--- Project/skinet/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ Project/skinet/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4351da6a291bfd738057f8bf3bd6e73ec97d04b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2491a44e580872092009b95535ac375fb9191627")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Project/skinet/Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+        public DbSet<Core.Entities.Product> Products { get; set; }
+    }
+}
