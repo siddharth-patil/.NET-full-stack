0037004e4ba7a0463144ac7a315a17340a5f7cca
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-09-28 22:29:41 +0530

    skinet - repository pattern implementation

--- Project/skinet/API/Program.cs
+++ Project/skinet/API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,6 +13,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 }); // Add your DbContext here
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>(); // Add your repository here
 
 var app = builder.Build();
 
---
+++ Project/skinet/Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        public void AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product?> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ProductExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
