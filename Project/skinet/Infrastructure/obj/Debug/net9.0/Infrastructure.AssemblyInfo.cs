13611dc325c5a1eb9b6a5f4aef7130641b963a10
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-11-08 20:01:36 +0530

    get api call for products

--- Project/skinet/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Project/skinet/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+361f586db5ff0bb8d9dde25a980989634b2b3217")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+970fd231bd295fbd9f8201405861223d86545e9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Project/skinet/client/src/app/app.ts
+++ Project/skinet/client/src/app/app.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,19 @@ import { HeaderComponent } from "./layout/header/header.component";
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit{
+  
+  baseUrl = 'https://localhost:7068/api/';
+  private http = inject(HttpClient);
   protected title = 'client';
+  products : Product[] = [];
+  // constructor(private http : HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error=>console.log(error),
+      complete: ()=> console.log('complete')
+    })
+  }
 }
