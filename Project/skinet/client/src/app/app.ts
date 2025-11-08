370a9e771ace45c0cd304cc305bc74a930f24f5b
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-11-08 22:26:57 +0530

    Shopping UI - Angular

--- Project/skinet/client/src/app/app.ts
+++ Project/skinet/client/src/app/app.ts
@@ -1,29 +1,32 @@
+import { ShopService } from './core/services/shop.service';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
 import { Product } from './shared/models/product';
 import { Pagination } from './shared/models/pagination';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements OnInit{
+export class App{
   
-  baseUrl = 'https://localhost:7068/api/';
-  private http = inject(HttpClient);
-  protected title = 'client';
-  products : Product[] = [];
+  // baseUrl = 'https://localhost:7068/api/';
+  // private http = inject(HttpClient);
+  // private shopService = inject(ShopService);
+  protected title = 'skinet';
+  // products : Product[] = [];
   // constructor(private http : HttpClient){}
 
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: response => this.products = response.data,
-      error: error=>console.log(error),
-      complete: ()=> console.log('complete')
-    })
-  }
+  // ngOnInit(): void {
+  //   // this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe
+  //   this.shopService.getProducts().subscribe({
+  //     next: response => this.products = response.data,
+  //     error: error=>console.log(error)
+  //   })
+  // }
 }
