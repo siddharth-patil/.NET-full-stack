c60b8fcd8a6d7e9f1cb47650befbf3189a04ec8d
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-11-10 11:38:20 +0530

    Angular shopping UI finished

--- Project/skinet/client/src/app/core/services/shop.service.ts
+++ Project/skinet/client/src/app/core/services/shop.service.ts
@@ -1,4 +1,5 @@
-import { HttpClient } from '@angular/common/http';
+import { ShopParams } from './../../shared/models/shopParams';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Pagination } from '../../shared/models/pagination';
 import { Product } from '../../shared/models/product';
@@ -12,10 +13,37 @@ export class ShopService {
   types: string[] = [];
   brands: string[] = [];
 
-  getProducts() {
+  // getProducts(brands?: string[], types?:string[], sort?:string) 
+  getProducts(shopParams:ShopParams) {
+
+    let params = new HttpParams();
+
+    if(shopParams.brands.length>0){
+      params = params.append('brands', shopParams.brands.join(','));
+    }
+
+    if(shopParams.types.length>0){
+      params = params.append('brands', shopParams.types.join(','));
+    }
+
+    if(shopParams.sort){
+      params = params.append('sort',shopParams.sort);
+    }
+
+    if(shopParams.search){
+      params = params.append('search',shopParams.search);
+    }
+
+    params = params.append('pageSize',shopParams.pageSize);
+    params = params.append('pageIndex',shopParams.pageNumber);
+    // return this.http.get<Pagination<Product>>(
+    //   this.baseUrl + 'products?pageSize=20'
+    // );
+
     return this.http.get<Pagination<Product>>(
-      this.baseUrl + 'products?pageSize=20'
+      this.baseUrl + 'products', {params}
     );
+
   }
 
   getBrands() {
