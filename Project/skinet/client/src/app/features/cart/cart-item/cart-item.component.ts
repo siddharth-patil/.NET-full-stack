4c44b6e94671e3372605737da6b074b0fa306b02
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-11-21 10:26:31 +0530

    cart details - UI

---
+++ Project/skinet/client/src/app/features/cart/cart-item/cart-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, input } from '@angular/core';
+import { CartItem } from '../../../shared/models/cart';
+import { RouterLink } from "@angular/router";
+import { MatButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-cart-item',
+  imports: [RouterLink, MatIcon, CurrencyPipe, MatButton],
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.scss'
+})
+export class CartItemComponent {
+  item = input.required<CartItem>();
+}
