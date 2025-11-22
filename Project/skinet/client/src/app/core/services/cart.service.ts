6d4bf69321cf66d915fe65545e6319fee46c2589
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-11-22 21:40:09 +0530

    Finishing UI for cart operations

--- Project/skinet/client/src/app/core/services/cart.service.ts
+++ Project/skinet/client/src/app/core/services/cart.service.ts
@@ -14,6 +14,16 @@ export class CartService {
   cart = signal<Cart | null>(null);
   itemCount = computed(()=>{
     return this.cart()?.items.reduce((sum, item) => sum + item.quantity,0)
+  });
+  totals = computed(()=>{
+    const cart = this.cart();
+    if (!cart) return null;
+    const subtotal = cart.items.reduce((sum, item) => sum+item.price * item.quantity, 0);
+    const shipping = 0;
+    const discount = 0;
+    return{
+      subtotal, shipping, discount, total:subtotal+shipping-discount
+    }
   })
 
   getCart(id:string){
@@ -41,6 +51,32 @@ export class CartService {
     }
     cart.items = this.addOrUpdateItem(cart.items, item, quantity);
     this.serCart(cart);
+  }
+
+  removeItemFromCart(productId: number, quantity=1){
+    const cart = this.cart();
+    if (!cart) return;
+    const index = cart.items.findIndex(x=>x.productId === productId);
+    if(index!==-1){
+      if (cart.items[index].quantity>quantity) {
+        cart.items[index].quantity -= quantity;
+      } else {
+        cart.items.splice(index,1);
+      }
+      if (cart.items.length === 0) {
+        this.deleteCart();
+      } else{
+        this.serCart(cart);
+      }
+    }
+  }
+  deleteCart() {
+    this.http.delete(this.baseUrl + 'cart?id=' + this.cart()?.id).subscribe({
+      next:()=>{
+        localStorage.removeItem('cart_id');
+        this.cart.set(null);
+      }
+    })
   }
   
   private addOrUpdateItem(items: CartItem[], item: CartItem, quantity: number): CartItem[] {
